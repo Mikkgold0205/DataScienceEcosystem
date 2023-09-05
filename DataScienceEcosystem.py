8cb5113645f9e8d28f2d02231490098d3de3be53
Mikkgold0205 <130360484+Mikkgold0205@users.noreply.github.com>
2023-09-05 20:58:40 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data scince Tools and Ecosystem are summarized.
+
+# In[2]:
+
+
+Some of the popular languages that Data Scientists use are:
+    1. JavaScript
+    2. PHP
+    3. Go
+    4. Ruby
+    5. Scala
+    6. C++
+    7. Java
+
+
+# In[1]:
+
+
+Some of the popular libraries that Data Scientists use are:
+    1. Panda 
+    2. Numpy
+    3. Matplotlib
+    4. Seaborn
+    5. Scikit-learn
+    
+
+
+# |S/n| Tools| Environment|
+# |:-:|:----:|:----------:|
+# |1|Jupyter notebook| create and share comment, equate and visualize|
+# |2|R studio|Unifies programming,Execute, Debugging,remote data access, Visalization|
+# |3|SQL|designing for managing data in relational database|
+# 
+# 
+
+# In[5]:
+
+
+a = 3
+b = 4
+c = 5
+
+print(a*b+c)
+
+
+# # convert minutes to hours
+
+# In[6]:
+
+
+minutes = 200 % 60
+hours = 200 //60
+
+
+
+
+
+print(hours)
+
+
+# ### Below are a few examples of evaluating arithmetic expressions in python
+
+# # Objectives:
+# 
+# * Javascript
+# * PHP
+# * RUBY
+# * GO
+# * Visual basic
+
+# # Author: Apelehin Adewunmi Olayemi
+# 
+
+# In[ ]:
+
+
+
+
